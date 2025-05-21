25e836148a0a03f27fc49c3ee9c2195c02f46cbe
Adrian Astalus <astalus.adrian1703@gmail.com>
2025-05-21 18:07:25 +0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="F1 Compatibility App", layout="wide")
 st.sidebar.title("Navigation")
-st.sidebar.page_link('pages/home.py', label="🏠 Home")
-st.sidebar.page_link("app/driverProfile.py", label="👤 Driver Profile")
-st.sidebar.page_link("app/compatibilityMatrix.py", label="🔗 Compatibility Matrix")
-st.sidebar.page_link("app/telemetryExplorer.py", label="📈 Telemetry Explorer")
+st.page_link('pages/home.py', label="🏠 Home")
+st.page_link("app/driverProfile.py", label="👤 Driver Profile")
+st.page_link("app/compatibilityMatrix.py", label="🔗 Compatibility Matrix")
+st.page_link("app/telemetryExplorer.py", label="📈 Telemetry Explorer")
