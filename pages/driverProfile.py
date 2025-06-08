f14144b5a777555e69ffcca439819dd8a8980ff9
Adrian Astalus <astalus.adrian1703@gmail.com>
2025-06-08 19:01:19 +0300

    Update driverProfile.py

--- pages/driverProfile.py
+++ pages/driverProfile.py
@@ -7,10 +7,9 @@ from src.data_ingestion.openf1_loader import *
 from src.data_ingestion.fastf1_loader import get_distinct_drivers
 from src.utils.df_utils import format_laptime
 
-
+@st.cache_data
 def get_drivers_list():
     drivers_list = get_distinct_drivers()
-
     return drivers_list
 
 
@@ -22,6 +21,9 @@ st.title("Driver Profile")
 # --- Sidebar Filters ---
 st.sidebar.header("Driver Selector")
 driver = st.sidebar.selectbox("Choose Driver", options=list_of_drivers)  # Example list
-season = st.sidebar.selectbox("Season", options=seasons)
-session_type = st.sidebar.selectbox("Session Type", options=["RACE", "QUALIFYING"])
-round_number = st.sidebar.number_input("Round (optional)", min_value=1, max_value=25, step=1)
+
+
+## not in the sidebar, make them choices similar to the homepage
+# season = st.sidebar.selectbox("Season", options=seasons)
+# session_type = st.sidebar.selectbox("Session Type", options=["RACE", "QUALIFYING"])
+# round_number = st.sidebar.number_input("Round (optional)", min_value=1, max_value=25, step=1)
