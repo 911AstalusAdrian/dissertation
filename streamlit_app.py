7ee2f1b57e353bfb6669f50c32ee94fb71115dc9
Adrian Astalus <astalus.adrian1703@gmail.com>
2025-06-12 12:31:28 +0300

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import fastf1
 
 st.set_page_config(page_title="F1 Compatibility App", layout="wide")
 
-fastf1.Cache.set_enabled()
 pages = [st.Page('pages/home.py', title = "Home"),
          st.Page('pages/driverProfile.py', title = 'Driver Profile'),
          st.Page('pages/compatibilityMatrix.py', title = 'Compatibility Matrix'),
