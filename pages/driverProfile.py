568dbb7531d49667565bc55a33aa9b93edeca50a
Adrian Astalus <astalus.adrian1703@gmail.com>
2025-06-12 12:09:38 +0300

    Update driverProfile.py

--- pages/driverProfile.py
+++ pages/driverProfile.py
@@ -7,19 +7,19 @@ from src.data_ingestion.openf1_loader import *
 from src.data_ingestion.fastf1_loader import get_distinct_drivers, get_driver_stats_multiseason
 from src.utils.df_utils import format_laptime
 
-@st.cache_data
+# @st.cache_data
 def get_drivers_data():
     drivers_list = get_distinct_drivers()
     return drivers_list
 
-@st.cache_data
+# @st.cache_data
 def get_driver_stats(driver_name):
     stats = get_driver_stats_multiseason(driver_full_name=driver_name)
     return stats
 
-@st.cache_data
-def get_driver_photo(driver_name):
-    pass
+# @st.cache_data
+# def get_driver_photo(driver_name):
+#     pass
 
 drivers_list = get_drivers_data()
 seasons = list(range(2018, 2026))
@@ -45,6 +45,5 @@ if show_driver_button:
 
     col3.metric("Total Points", driver_stats['Points'])
     col3.metric("Avg Points per Race", driver_stats['Avg Points/Race'])
-    col3.metric('Headshot URL', driver_stats['HeadshotUrl'])
     # load_data regarding the driver using openF1 API '/driver'
     # make sure to modify the last name to be all caps
