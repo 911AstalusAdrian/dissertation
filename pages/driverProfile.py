007c662869fa4de9c00c0cca40412cd8aa76a9d1
Adrian Astalus <astalus.adrian1703@gmail.com>
2025-06-14 11:35:17 +0300

    Fix imports

--- pages/driverProfile.py
+++ pages/driverProfile.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 from src.data_ingestion.openf1_loader import get_driver_image
-from src.data_ingestion.fastf1_loader import get_driver_full_info, get_events_for_season, get_distinct_drivers, get_driver_stats_multiseason, get_race_results_over_seasons, get_driver_teammate_comparison_over_seasons
+from src.data_ingestion.fastf1_loader import get_driver_full_info, get_distinct_drivers
 from src.utils.plot_utils import TEAM_COLORS
 
 @st.cache_data(show_spinner='Getting the list of available drivers...')
